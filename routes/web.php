0493b74bb21769496140d3ff4438c139ad0d38d4
summeryio <1005526074@qq.com>
2020-05-23 17:37:15 +0800

    用户界面 - 商品详情页

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,4 @@ Route::group(['middleware' => ['auth', 'verified']], function () {
 
 // 商品列表
 Route::get('products', 'ProductsController@index')->name('products.index');
+Route::get('products/{product}', 'ProductsController@show')->name('products.show');
