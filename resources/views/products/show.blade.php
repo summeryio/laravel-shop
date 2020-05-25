74d588d46aa78feac74fc521b3d21265c5ce8df7
summeryio <1005526074@qq.com>
2020-05-25 14:51:04 +0800

    用户界面 - 商品收藏

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -36,7 +36,11 @@
                             </div>
                             <div class="cart_amount"><label>数量</label><input type="text" class="form-control form-control-sm" value="1"><span>件</span><span class="stock"></span></div>
                             <div class="buttons">
+                                @if($favored)
+                                <button class="btn btn-success btn-disfavor">取消收藏</button>
+                                @else
                                 <button class="btn btn-success btn-favor">❤ 收藏</button>
+                                @endif
                                 <button class="btn btn-primary btn-add-to-cart">加入购物车</button>
                             </div>
                         </div>
@@ -73,6 +77,32 @@
                 $('.product-info .price span').text($(this).data('price'));
                 $('.product-info .stock').text('库存：' + $(this).data('stock') + '件');
             });
+        });
+
+        $('.btn-favor').click(function () {
+            // 发起一个 post ajax 请求，请求 url 通过后端的 route() 函数生成。
+            axios.post('{{ route('products.favor', ['product' => $product->id]) }}')
+                .then(function () { // 请求成功会执行这个回调
+                    console.log('收藏成功');
+                }, function(error) { // 请求失败会执行这个回调
+                    // 如果返回码是 401 代表没登录
+                    if (error.response && error.response.status === 401) {
+                        console.log('请先登录');
+                    } else if (error.response && (error.response.data.msg || error.response.data.message)) {
+                        // 其他有 msg 或者 message 字段的情况，将 msg 提示给用户
+                        console.log(error.response.data.msg ? error.response.data.msg : error.response.data.message);
+                    }  else {
+                        // 其他情况应该是系统挂了
+                        console.log('系统错误');
+                    }
+                });
+        });
+
+        $('.btn-disfavor').click(function () {
+            axios.delete('{{ route('products.disfavor', ['product' => $product->id]) }}')
+                .then(function () {
+                    console.log('取消收藏成功')
+                });
         });
     </script>
 @endsection
