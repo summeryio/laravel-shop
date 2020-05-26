fc42c84cecd67535ae866d48536274467771a1f9
summeryio <1005526074@qq.com>
2020-05-26 18:32:19 +0800

    update

--- routes/web.php
+++ routes/web.php
@@ -26,6 +26,9 @@ Route::group(['middleware' => ['auth', 'verified']], function () {
     Route::post('cart', 'CartController@add')->name('cart.add');
     Route::get('cart', 'CartController@index')->name('cart.index');
     Route::delete('cart/{sku}', 'CartController@remove')->name('cart.remove');
+
+    // 下单
+    Route::post('orders', 'OrdersController@store')->name('orders.store');
 });
 
 
