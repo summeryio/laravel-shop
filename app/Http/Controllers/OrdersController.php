945eb7c2e26423d6718aca63313ffbbcbc1b41a1
summeryio <1005526074@qq.com>
2020-05-27 14:31:44 +0800

    关闭未支付订单

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Exceptions\InvalidRequestException;
 use App\Http\Requests\OrderRequest;
+use App\Jobs\CloseOrder;
 use App\Models\Order;
 use App\Models\ProductSku;
 use App\Models\UserAddress;
@@ -56,6 +57,9 @@ class OrdersController extends Controller
 
             $skuIds = collect($items)->pluck('sku_id');
             $user->CartItems()->whereIn('product_sku_id', $skuIds)->delete();
+
+            // 触发任务
+            $this->dispatch(new CloseOrder($order, config('app.order_ttl')));
 
             return $order;
         });
