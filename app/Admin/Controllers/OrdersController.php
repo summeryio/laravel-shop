7f73bd0f24c1598a95e14a5bf37b99d5174b7014
summeryio <1005526074@qq.com>
2020-05-29 13:10:05 +0800

    管理后台 - 订单发货

--- app/Admin/Controllers/OrdersController.php
+++ app/Admin/Controllers/OrdersController.php
@@ -2,13 +2,18 @@
 
 namespace App\Admin\Controllers;
 
+use App\Exceptions\InvalidRequestException;
+use Illuminate\Http\Request;
 use App\Models\Order;
 use Encore\Admin\Controllers\AdminController;
 use Encore\Admin\Grid;
 use Encore\Admin\Layout\Content;
+use Illuminate\Foundation\Validation\ValidatesRequests;
 
 class OrdersController extends AdminController
 {
+    use ValidatesRequests;
+
     protected $title = '订单';
 
     protected function grid()
@@ -43,5 +48,30 @@ class OrdersController extends AdminController
         return $content
             ->header('查看订单')
             ->body(view('admin.orders.show', ['order' => Order::find($id)]));
+    }
+
+    public function ship(Order $order, Request $request) {
+        if (!$order->paid_at) {
+            throw new InvalidRequestException('该订单未付款');
+        }
+
+        if ($order->ship_status !== Order::SHIP_STATUS_PENDING) {
+            throw new InvalidRequestException('该订单已发货');
+        }
+
+        $data = $this->validate($request, [
+            'express_company' => ['required'],
+            'express_no' => ['required'],
+        ], [], [
+            'express_company' => '物流公司',
+            'express_no' => '物流单号',
+        ]);
+
+        $order->update([
+            'ship_status' => Order::SHIP_STATUS_DELIVERED,
+            'ship_data' => $data,
+        ]);
+
+        return redirect()->back();
     }
 }
