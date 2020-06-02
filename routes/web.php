61b87b03ef05ecd392e3459db2c74dced11d0a17
summeryio <summeryio@summeryiodeiMac.local>
2020-06-02 20:26:26 +0800

    用户界面 - 检查优惠券

--- routes/web.php
+++ routes/web.php
@@ -45,6 +45,9 @@ Route::group(['middleware' => ['auth', 'verified']], function () {
 
     // 用户申请退款
     Route::post('orders/{order}/apply_refund', 'OrdersController@applyRefund')->name('orders.apply_refund');
+
+    // 优惠券
+    Route::get('coupon_codes/{code}', 'CouponCodesController@show')->name('coupon_codes.show');
 });
 
 
